21ec58d1e99cec2b54b1cf8543bafe15408ae2cf
Kevin Hernandez <daniel.ihernandez@mercadolibre.com.mx>
2023-12-26 22:12:58 -0600

    feat: add fileMock, mockery files

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,4 +4,8 @@ module.exports = {
   // collectCoverage: true,
   // collectCoverageFrom: ['src/**/*.js'],
   // coverageReporters: ['lcov', 'text-summary'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+  },
 };
